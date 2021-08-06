e2a81f063940b2f322634a5bb0aea378f8d64da2
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-06 16:33:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -457,15 +457,24 @@ friends = ["Jim", "Karen", "Sally", "John", "Mike"]
 # result = cube(4)
 # print(result)
 
-is_male = True
-is_tall = False
+# is_male = True
+# is_tall = False
+#
+# if is_male and is_tall:
+#     print("Person is a tall male.")
+# elif is_male and not(is_tall):
+#     print("Person is a short male.")
+# elif not(is_male) and is_tall:
+#     print("Person is tall, but not a male.")
+# else:
+#     print("Person is not male and not tall.")
 
-if is_male and is_tall:
-    print("Person is a tall male.")
-elif is_male and not(is_tall):
-    print("Person is a short male.")
-elif not(is_male) and is_tall:
-    print("Person is tall, but not a male.")
-else:
-    print("Person is not male and not tall.")
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
 
+print(max_num(3, 400, 5))
