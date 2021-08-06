10739ccb308729bf98e5e8f675363f3c3625598d
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-06 16:45:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -494,3 +494,19 @@ friends = ["Jim", "Karen", "Sally", "John", "Mike"]
 # else:
 #     print("Invalid operator.")
 
+monthConversions = {
+    "Jan" : "January",
+    "Feb" : "February",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sep" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December"
+}
+
+print(monthConversions.get("Luv", "Key not found"))
