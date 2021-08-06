1799f23a465d5262ab172634cf47d6466b1c93fb
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-06 17:07:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -550,3 +550,16 @@ friends = ["Jim", "Karen", "Sally", "John", "Mike"]
 #
 # print(raise_to_power(2, 3))
 
+# number_grid = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9],
+#     [0]
+# ]
+#
+# # print(number_grid[2][1])
+#
+# for row in number_grid:
+#     for col in row:
+#         print(col)
+
