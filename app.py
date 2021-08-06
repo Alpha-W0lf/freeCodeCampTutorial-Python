fa528724fd8387e69c115a1134f3fdc0b9088c39
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-06 16:39:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -469,12 +469,28 @@ friends = ["Jim", "Karen", "Sally", "John", "Mike"]
 # else:
 #     print("Person is not male and not tall.")
 
-def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
-        return num3
+# def max_num(num1, num2, num3):
+#     if num1 >= num2 and num1 >= num3:
+#         return num1
+#     elif num2 >= num1 and num2 >= num3:
+#         return num2
+#     else:
+#         return num3
+#
+# print(max_num(3, 400, 5))
 
-print(max_num(3, 400, 5))
+# num1 = float(input("Enter first number: "))
+# op = input("Enter operator: ")
+# num2 = float(input("Enter second number: "))
+#
+# if op == "+":
+#     print(num1 + num2)
+# elif op == "-":
+#     print(num1 - num2)
+# elif op == "/":
+#     print(num1 / num2)
+# elif op == "*":
+#     print(num1 * num2)
+# else:
+#     print("Invalid operator.")
+
