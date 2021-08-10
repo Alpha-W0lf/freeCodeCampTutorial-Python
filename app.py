81031bb89390883d39f15f894e7f1145f2b42128
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-10 11:37:18 -0500

    learning about reading/writing external files

--- app.py
+++ app.py
@@ -592,3 +592,10 @@ friends = ["Jim", "Karen", "Sally", "John", "Mike"]
 # except ValueError:
 #     print("Invalid input")
 
+# employee_file = open("employees.txt", "r")
+#
+# for employee in employee_file.readlines():
+#     print(employee)
+#
+# employee_file.close()
+
