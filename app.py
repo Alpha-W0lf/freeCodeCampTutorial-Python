4cf5cd44f0fdca14506f41cb96fa8f9cdc735a0e
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-10 11:44:20 -0500

    more reading/writing/appending external files

--- app.py
+++ app.py
@@ -599,8 +599,8 @@ friends = ["Jim", "Karen", "Sally", "John", "Mike"]
 #
 # employee_file.close()
 
-employee_file = open("employees.txt", "a")
+employee_file = open("index.html", "w")
 
-employee_file.write("\nToby - Human Resources")
+employee_file.write("<p>This is HTML</p>")
 
 employee_file.close()
