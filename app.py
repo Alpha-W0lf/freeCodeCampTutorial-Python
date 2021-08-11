053f16f41f86ca343856bd48f97630aed69cf481
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-11 11:47:34 -0500

    object functions

--- app.py
+++ app.py
@@ -639,3 +639,10 @@ friends = ["Jim", "Karen", "Sally", "John", "Mike"]
 #
 # run_test(questions)
 
+from Student1 import Student
+
+student1 = Student("Oscar", "Accounting", 3.1)
+student2 = Student("Phyllis", "Business", 3.8)
+
+print(student1.on_honor_roll())
+
