ff4e51e73d02ec2e820f10859f7c830434a6010f
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-11 11:00:00 -0500

    reviewing classes and objects

--- app.py
+++ app.py
@@ -610,3 +610,8 @@ friends = ["Jim", "Karen", "Sally", "John", "Mike"]
 # print(useful_tools.roll_dice(10))
 
 from Student1 import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 2.5, True)
+
+print(student1.gpa)
